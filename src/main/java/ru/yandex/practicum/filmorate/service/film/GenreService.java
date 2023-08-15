ab0ef0091496c02869c20a1f2598f3dc60f774ce
ZeroX <ZeroXKiitsu@yandex.ru>
2023-08-16 00:44:00 +0300

    refactor: deleted unused annotations

--- src/main/java/ru/yandex/practicum/filmorate/service/film/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/GenreService.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service.film;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
@@ -9,7 +8,6 @@ import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
 import java.util.List;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class GenreService {
 
--- src/main/java/ru/yandex/practicum/filmorate/service/film/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/MpaService.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service.film;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
@@ -9,7 +8,6 @@ import ru.yandex.practicum.filmorate.storage.mpa.MpaStorage;
 import java.util.List;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class MpaService {
     private final MpaStorage mpaStorage;
